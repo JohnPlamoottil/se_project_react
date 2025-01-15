ddbb42311bdb1d2c2a65bdd40f89fb5b001b1d97
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-01-14 18:01:45 -0600

    Part6 Rendering and Filtering Cards Completed Jan14 6.01pm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,22 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 
+import { useState } from "react";
+
 import "./App.css";
-import Main from "../Main/Main";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+  // const [weatherData, setWeatherData] = useState({ type: "hot" });
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
         <Footer />
       </div>
     </div>
--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,2 +1,15 @@
+function ItemCard({ item }) {
+  //   console.log(props.item);
+  return (
+    <div>
+      <h2>{item.name}</h2>
+      <img src={item.link} alt={item.name} />
+    </div>
+  );
+}
+
+export default ItemCard;
+
+// Pheonix informed me of unsplash to get pictures for W2W
 // src "https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D"
 // red blouse
