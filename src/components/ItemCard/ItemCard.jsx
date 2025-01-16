999b78d4204c83d23794e92eef7884423a4928a0
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-01-15 18:14:19 -0600

    Project 10 Part 7 - Styling Cards Completed

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,15 +1,19 @@
+import "./ItemCard.css";
+
 function ItemCard({ item }) {
   //   console.log(props.item);
   return (
-    <div>
-      <h2>{item.name}</h2>
-      <img src={item.link} alt={item.name} />
-    </div>
+    <li className="card">
+      <h2 className="card__name">{item.name}</h2>
+      <img className="card__image" src={item.link} alt={item.name} />
+    </li>
   );
 }
 
 export default ItemCard;
 
-// Pheonix informed me of unsplash to get pictures for W2W
+// Phoenix informed me of unsplash to get pictures for W2W
 // src "https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D"
 // red blouse
+
+// change div to li because we are using ul on line14 of Main.jsx
