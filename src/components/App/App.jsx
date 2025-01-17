1632e09687ea8d8d8047f49d117c242095c2a785
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-01-17 15:56:37 -0600

    Project 10 Part 8 - ModalWithForm Markup and Styles Completed Jan16 8am to Jan17 3.55pm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,10 @@
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-
 import { useState } from "react";
 
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
@@ -19,8 +17,12 @@ function App() {
         <Main weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm />
     </div>
   );
 }
 
 export default App;
+
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
