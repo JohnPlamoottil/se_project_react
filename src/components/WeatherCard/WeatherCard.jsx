5f5e6ba08722518ca6d2087207c2d46731652695
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-02-05 07:30:00 -0600

    Part 14 Dynamic Weather Cards 15min mark completed Feb4-5 2025

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,29 @@
 import "./WeatherCard.css";
+import { weatherOptions } from "../../utils/constants";
+
 import sunny from "../../assets/images/day/sunny.png";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+  // since weatherOption is a array with ONE element not an option, need the 0th element
+  const weatherOptionUrl = filteredOptions[0]?.url;
+  // quesiton mark is for optional chaining at 14min10sec of video 14
+  const weatherOptionCondition = filteredOptions[0]?.condition;
+  const weatherOptionDay = filteredOptions[0]?.isDay;
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
-      <img src={sunny} alt="sunny" className="weather-card__image" />
+      <img
+        src={weatherOptionUrl}
+        alt={`Card showing ${weatherOptionDay} ${weatherOptionCondition}weather`}
+        className="weather-card__image"
+      />
     </section>
   );
 }
@@ -15,3 +33,4 @@ export default WeatherCard;
 //how to research props to react component Jan27
 //send the data as prop"weatherData" to weatherCard.jsx Jan27
 //weatherData call it that and pass it Jan27
+// Feb4 2am and 6:27 and 7am Part14 video
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,76 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rain.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snow.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "storm",
+    url: new URL("../assets/day/storm.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "sunny",
+    url: new URL("../assets/day/sunny.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../assets/night/fog.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snow.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "storm",
+    url: new URL("../assets/night/storm.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "sunny",
+    url: new URL("../assets/night/sunny.png", import.meta.url).href,
+  },
+];
+
 export const defaultClothingItems = [
   {
     _id: 0,
