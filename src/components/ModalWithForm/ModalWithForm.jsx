0ab0081dfe872b736cedb24c90a28f2a6abb7005
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-02-12 04:44:31 -0600

    Kevin helped me on Feb11 9-15pm CST on correcting the close logo on modal and adding description onto the itemCards

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,4 +1,5 @@
 import "./ModalWithForm.css";
+import closeIcon from "../../assets/close.png";
 
 function ModalWithForm({
   children,
@@ -13,7 +14,7 @@ function ModalWithForm({
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onClose} type="button" className="modal__close">
-          CLOSE
+          <img src={closeIcon} alt="close icon" />
         </button>
         <form className="modal__form">
           {children}
