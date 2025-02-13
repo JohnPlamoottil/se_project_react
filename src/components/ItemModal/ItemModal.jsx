a45dd144a24855f1bbe6f30af33bb0e9fac03054
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-02-13 09:28:41 -0600

    close icon on Item Cards by removing close tag. Feb13 Alberto.

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,11 +1,12 @@
 import "./ItemModal.css";
+import closeIcon from "../../assets/close.png";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
-          CLOSE
+          <img src={closeIcon} alt="close icon" />
         </button>
         <img src={card.link} alt="modal image" className="modal__image" />
 
