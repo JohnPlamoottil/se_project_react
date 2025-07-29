fdf25439f3000e3de488ada3eba84419ced56c42
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-07-29 17:02:52 -0500

    setup auth.js complete

---
+++ src/utils/auth.js
@@ -0,0 +1,31 @@
+const baseUrl = "http://localhost:3001";
+
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Error: ${res.status}`);
+  }
+}
+
+function signup({ name, email, password, avatarURL }) {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar: avatarURL, email, password }),
+  }).then(checkResponse);
+}
+
+function signin({ email, password }) {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+}
+
+export { signup, signin };
