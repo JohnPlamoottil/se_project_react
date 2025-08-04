2fdfe1c1d815b9cc01cbd4ec4be8a15e284849c6
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-08-04 17:45:17 -0500

    remove duplicate ItemModal lines291-295 and remove notes from the bottom after moving them to prj14 instruction word doc

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -288,11 +288,7 @@ const App = () => {
               setActiveModal("delete");
             }}
           />
-          <ItemModal
-            activeModal={activeModal}
-            card={selectedCard}
-            onClose={closeActiveModal}
-          />
+
           <DeleteConfirmationModal
             onClose={() => {
               setActiveModal("");
@@ -313,80 +309,3 @@ const App = () => {
 };
 
 export default App;
-
-// Notes
-
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-
-/* Render <AddItemModal /> and pass it props */
-
-/* // May15 Serge said to render AddItem Modal and comment out ModalWithForm */
-
-/* <ModalWithForm
-          title="New Garment"
-          buttonText="Add Garment"
-          onClose={closeActiveModal}
-          isOpen={activeModal === "add-garment"}
-        >
-          <label htmlFor="name" className="modal__label">
-            Name{" "}
-            <input
-              type="text"
-              className="modal__input"
-              id="name"
-              placeholder="Name"
-            />
-          </label>
-          <label htmlFor="imageUrl" className="modal__label">
-            Image{" "}
-            <input
-              type="text"
-              className="modal__input"
-              id="imageUrl"
-              placeholder="Image URL"
-            />
-          </label>
-          <fieldset className="modal__radio-buttons">
-            <legend className="modal__legend">Select the weather type;</legend>
-            <label
-              htmlFor="hot"
-              className="modal__input modal__label_type_radio"
-            >
-              <input
-                id="hot"
-                type="radio"
-                value="hot"
-                name="radio"
-                className="modal__radio-input"
-              />{" "}
-              Hot
-            </label>
-            <label
-              htmlFor="warm"
-              className="modal__input modal__label_type_radio"
-            >
-              <input
-                id="warm"
-                type="radio"
-                value="warm"
-                name="radio"
-                className="modal__radio-input"
-              />{" "}
-              Warm
-            </label>
-            <label
-              htmlFor="cold"
-              className="modal__input modal__label_type_radio"
-            >
-              <input
-                id="cold"
-                type="radio"
-                value="cold"
-                name="radio"
-                className="modal__radio-input"
-              />{" "}
-              Cold
-            </label>
-          </fieldset>
-        </ModalWithForm> */
