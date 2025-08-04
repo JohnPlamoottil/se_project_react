40fa5182410c66902936942ab58f981f8056ca17
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-08-04 17:31:39 -0500

    remove some notes from itemCard jsx file

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -43,9 +43,3 @@ const ItemCard = ({ card, onCardClick, onCardLike }) => {
 };
 
 export default ItemCard;
-
-// Phoenix informed me of unsplash to get pictures for W2W
-// src "https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D"
-// red blouse
-
-// change div to li because we are using ul on line14 of Main.jsx
